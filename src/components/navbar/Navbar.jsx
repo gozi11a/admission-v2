b44ad63380f906ce56e6a970cbcaf825a58caf9b
gozi11a <behruz.jrw@gmail.com>
2024-07-29 02:19:53 -0700

    fixed by behruz

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -4,10 +4,8 @@ import Navbar from "react-bootstrap/Navbar";
 import NavDropdown from "react-bootstrap/NavDropdown";
 import MainLogo from "/src/assets/logo/LogoWhite.png";
 import "./navbar.scss";
-import { useTranslation } from "react-i18next";
 
 function BasicExample() {
-  const { tr } = useTranslation();
   return (
     <Navbar expand="lg" className="bg-body-tertiary customNavbar">
       <Container>
@@ -17,7 +15,7 @@ function BasicExample() {
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link href="#home">{tr("navlink1")}UY</Nav.Link>
+            <Nav.Link href="#home">UY</Nav.Link>
             <Nav.Link href="#link">About</Nav.Link>
             <Nav.Link href="#link">Contact</Nav.Link>
             <Nav.Link href="#link">Support</Nav.Link>
